944d30f90e1637262530f304adc251a9a17210c2
Steve Teng <steveteng@nbnco.com.au>
2014-07-30 12:01:00 +1000

    s

--- build/resources/main/scripts/post_install_conf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-APP_NAME="springseed"
-USERID=$APP_NAME
-
-chown -R $USERID:$USERID /opt/nbnco/applications/$APP_NAME
-
-/sbin/chkconfig --add $APP_NAME
-/sbin/chkconfig $APP_NAME on
-/etc/init.d/$APP_NAME start
--- build/resources/main/scripts/pre_uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-/etc/init.d/springseed stop
-/sbin/chkconfig --del springseed
-
-#for i in `find /tmp -type d -name '*ehour.war*'`; do
-#  rm -rf $i
-#done
